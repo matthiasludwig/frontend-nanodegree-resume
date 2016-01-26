8013b14bb20d95c8443398069d885211113832a7
Matthias Ludwig <matthiasludwig@me.com>
2016-01-25 16:20:37 -0800

    Added JSON, Functions, Google Map and changed the CSS of the Resume Template

--- js/helper.js
+++ js/helper.js
@@ -20,7 +20,8 @@ var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedin = '<li class="flex-item"><span class="orange-text">LinkedIn</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">homepage</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
@@ -53,7 +54,7 @@ var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><a href="#" id="linkSchool">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -65,13 +66,13 @@ The International Name challenge in Lesson 2 where you'll create a function that
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
+
 clickLocations = [];
 
 function logClicks(x,y) {
@@ -90,7 +91,7 @@ $(document).click(function(loc) {
 
   logClicks(x,y);
 });
-
+*/
 
 
 /*
@@ -112,9 +113,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -133,7 +134,7 @@ function initializeMap() {
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     education.schools.forEach(function(school){
       locations.push(school.location);
@@ -141,7 +142,7 @@ function initializeMap() {
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     work.jobs.forEach(function(job){
       locations.push(job.location);
@@ -241,11 +242,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,15 +1,77 @@
-//Header formatting
+/*
+The document follows the following structure
+1. JSON Object
+2. Function Decleration
+3. Calling of the function (declared in 2.)
+*/
 
-var name = "Matthias Ludwig";
-var role = "Business Geek";
+var bio = {
+    "name" : "Matthias Ludwig",
+    "role" : "Business Geek",
+    "welcomemessage" : "Welcome to my resume!",
+    "contacts" : {
+        "mobile" : "(415) 483-6433",
+        "email" : "matthiasludwig@me.com",
+        "github" : "matthiasludwig",
+        "linkedin" : "/in/ludwigmatthias",
+        "homepage" : "www.matthiasludwig.me",
+        "twitter" : "matthiasludw",
+        "location" : "San Francisco, CA"
+    },
+    "skills" : ["Business", "Programming", "Front-End Web Development"],
+    "biopic" : "images/matthias.jpg"
+}
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+bio.display = function()
+{
+    //Formatting the Data from the JSON with function from the helper.js file
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
+    var formattedHomepage = HTMLblog.replace("%data%", bio.contacts.homepage);
+	var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	var formattedBiopic = HTMLbioPic.replace("%data%", bio.biopic);
+	var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomemessage);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+    //Header Section of the Resume with Data
+	$('#header').prepend(formattedRole);
+	$('#header').prepend(formattedName);
+	$('#topContacts').append(formattedMobile);
+	$('#topContacts').append(formattedEmail);
+	$('#topContacts').append(formattedGithub);
+    $('#topContacts').append(formattedLinkedin);
+    $('#topContacts').append(formattedHomepage);
+	$('#topContacts').append(formattedTwitter);
+	$('#topContacts').append(formattedLocation);
+	$('#header').append(formattedBiopic);
+	$('#header').append(formattedWelcome);
 
-// Resume Objects
+    // Footer Section with Contact data
+	$('#footerContacts').append(formattedMobile);
+	$('#footerContacts').append(formattedEmail);
+	$('#footerContacts').append(formattedGithub);
+    $('#footerContacts').append(formattedLinkedin);
+    $('#footerContacts').append(formattedHomepage);
+	$('#footerContacts').append(formattedTwitter);
+	$('#footerContacts').append(formattedLocation);
+
+    //Adding Skills from the skills Array to the header
+    if (bio.skills.length > 0)
+    {
+        $('#header').append(HTMLskillsStart);
+
+        for (skill in bio.skills)
+        {
+            var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+            $("#skills").append(formattedSkill);
+        }
+    }
+}
+
 
 var work = {
     "jobs" : [
@@ -18,48 +80,83 @@ var work = {
             "title" : "Intern Business Development",
             "location" : "San Francisco, CA",
             "dates" : "09/2015 to present",
-            "description" : "Testtext for the job at HoloBuilder Inc."
+            "description" : "Worked closely with the VP of Business Development and Product on various assignments during the expansion of the company from Germany to the US."
         },
         {
-        "employer" : "Consiglia e.V.",
+            "employer" : "Consiglia e.V.",
             "title" : "Head of IT",
             "location" : "Saarbruecken, Germany",
-            "dates" : "04/2011 until 10/2014",
-            "description" : "Testtext for job at Consiglia e.V."
+            "dates" : "02/2012 until 10/2014",
+            "description" : "Administered the internal IT infrastructure that included the homepage, Facebook page and E-Mail Accounts. Modeled and improved the processes that apply to all departments, as well as during consulting projects. Led different project groups to continuously improve the organization and find new business opportunities."
+        },
+        {
+            "employer" : "KWT - Office for Knowledge and Technology Transfer of Saarland University",
+            "title" : "Student Assistant",
+            "location" : "Saarbruecken, Germany",
+            "dates" : "04/2013 until 08/2013",
+            "description" : "Supported entrepreneurs with different educational backgrounds on how to start and grow their business. Advised students on business development workshops and scholarships that best suited their needs."
+        },
+        {
+            "employer" : "German-European Law Library (Deutsch-Europäisches Juridicum)",
+            "title" : "Student Assistant",
+            "location" : "Saarbruecken, Germany",
+            "dates" : "09/2011 until 03/2014",
+            "description" : "Helped law students and professionals with their research needs for books and online databases. Answered user’s questions and helped them to find the right resources. Developed a Facebook page for communication and user support purposes."
         }
     ]
 }
+
+work.display = function()
+{
+    for (job in work.jobs)
+    {
+        $('#workExperience').append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedJobTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedJobLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        var formattedJobDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedJobDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+        var formattedEmployerTitle = formattedEmployer + formattedJobTitle + formattedJobLocation + formattedJobDates + formattedJobDescription;
+
+        $('.work-entry:last').append(formattedEmployerTitle);
+    }
+}
+
 
 var projects = {
     "projects" : [
         {
-            "title" : "Project A",
-            "dates" : "2013 until 2014",
-            "description" : "I did Project A",
-            "images" : ["images/projectA.jpg"]
+            "title" : "Boersenwochenende",
+            "dates" : "2013",
+            "description" : "The Börsenwochenende organized by the Börseninitiative Saar e.V. is an excursion to different financial institutions in Frankfurt a. Main, Germany. The goal is to put students in touch with financial companies and to gain new insights from the financial sector.",
+            "images" : ["images/boersenwochenende.jpg"]
         },
         {
-            "title" : "Project B",
-            "dates" : "2014 until 2015",
-            "description" : "I did Project B afterwards.",
-            "images" : ["images/projectB.jpg"]
+            "title" : "Consiglia Homepage",
+            "dates" : "2014",
+            "description" : "Transferring of the current homepage to a homepage to Squarespace",
+            "images" : ["images/consigliahomepage.jpg"]
         }
     ]
 }
+projects.display = function()
+{
+    for (project in projects.projects)
+    {
+        $('#projects').append(HTMLprojectStart);
 
-var bio = {
-    "name" : "Matthias Ludwig",
-    "role" : "Business Geek",
-    "welcome-message" : "Welcome to my resume!",
-    "contacts" : {
-        "mobile" : "(415) 483-6433",
-        "email" : "matthiasludwig@me.com",
-        "github" : "matthiasludwig",
-        "twitter" : "matthiasludw",
-        "location" : "San Francisco, CA"
-    },
-    "skills" : ["Business", "Programming", "Front-End Web Development"]
+        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images);
+
+        var formattedProject = formattedProjectTitle + formattedProjectDates + formattedProjectDescription + formattedProjectImage;
+        $('.project-entry:last').append(formattedProject);
+    }
 }
+
 
 var education = {
     "schools" : [
@@ -68,7 +165,7 @@ var education = {
             "location" : "San Francisco, CA",
             "degree" : "Master's Degree",
             "majors" : "International Business",
-            "dates" : 2015,
+            "dates" : "2014 until 2015",
             "url" : "www.hult.edu"
         },
         {
@@ -76,7 +173,7 @@ var education = {
             "location" : "Saarbruecken, Germany",
             "degree" : "Bachelor's Degree",
             "majors" : "Business and Law",
-            "dates" : 2014,
+            "dates" : "2009 until 2014",
             "url" : "www.uni-saarland.de"
         }
     ],
@@ -84,81 +181,79 @@ var education = {
         {
             "title" : "FEND Nanodegree",
             "school" : "Udacity",
-            "dates" : 2016,
+            "dates" : "2016",
             "url" : "www.udacity.com"
         },
         {
             "title" : "Introduction to Computer Science",
             "school" : "Harvard on edX",
-            "dates" : 2016,
+            "dates" : "2016",
             "url" : "www.edx.org"
         }
     ]
 }
 
-/* Lesson 2 - Control Flow */
-
-function displayWork()
+education.display = function()
 {
-
-if (bio.skills.length > 0)
-{
-    $('#header').append(HTMLskillsStart);
-
-    for (skill in bio.skills)
+    for (school in education.schools)
     {
-        var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-        $("#skills").append(formattedSkill);
+        $('#education').append(HTMLschoolStart);
+
+        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        var formattedSchoolUrl = HTMLonlineURL.replace("%data%", education.schools[school].url);
+
+        $('.education-entry:last').append(formattedSchoolName + formattedSchoolDegree);
+        $('.education-entry:last').append(formattedSchoolLocation);
+        $('.education-entry:last').append(formattedSchoolDates);
+        $('.education-entry:last').append(formattedSchoolMajor);
+        $('.education-entry:last').append(formattedSchoolUrl);
+    }
+
+    $('.education-entry:last').append(HTMLonlineClasses);
+
+    for (online in education.onlineCourses)
+    {
+        var formattedOnlineName = HTMLonlineTitle.replace("%data%", education.onlineCourses[online].title);
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[online].school);
+        var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[online].dates);
+        var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[online].url);
+
+        var formattedOnline = formattedOnlineName + formattedOnlineSchool + formattedOnlineDates + formattedOnlineURL;
+        $('.education-entry:last').append(formattedOnline);
     }
 }
 
-for (job in work.jobs)
-{
-    $('#workExperience').append(HTMLworkStart);
+//Call the function declared with the JSON above to make the content visible in the HTMLemail
 
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedJobTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedJobLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-    var formattedJobDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    var formattedJobDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+bio.display();
+work.display();
+projects.display();
+education.display();
+$("#mapDiv").append(googleMap);
 
-    var formattedEmployerTitle = formattedEmployer + formattedJobTitle + formattedJobLocation + formattedJobDates + formattedJobDescription;
-    $('.work-entry:last').append(formattedEmployerTitle);
-}
-}
 
-displayWork();
+/*
+# International Name function
+#
 
 $('#main').append(internationalizeButton);
 
-function inName(oldname) {
+function inName(name) {
 
-    var finalName = oldName;
+    var finalName = name;
 
     var names = oldName.split(" ");
     names[1] = names[1].toUpperCase();
     names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
     finalName = names.join(" ");
-    
+
     return finalName;
 }
-
-projects.display = displayProjects()
-{
-    for (project in projects.projects)
-{
-    $('#projects').append(HTMLprojectStart);
-
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedJobTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedJobLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-    var formattedJobDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    var formattedJobDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-
-    var formattedEmployerTitle = formattedEmployer + formattedJobTitle + formattedJobLocation + formattedJobDates + formattedJobDescription;
-    $('.work-entry:last').append(formattedEmployerTitle);
-}
-}
+*/
 
 
 /*
